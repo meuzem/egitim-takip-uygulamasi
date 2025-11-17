6373851e29312fce5d4a186f8b31917a1ab9c758
meuzem <meltemuzem@gmail.com>
2025-11-17 14:39:05 +0300

    feat: add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    GOOGLE_SHEETS_ID: '1ywjQGfHS6k4hqNiitkOUZ2HytTnu7N9Z5PO9A_yXMUY',
+  }
+}
+
+module.exports = nextConfig
